2b2d5bfa0190f120202b4ebe59d297b00ed22d77
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2024-08-19 17:14:46 -0300

    adicionado helloreact

---
+++ helloreact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='titulo1'> hello react js </h1>
+      <p className='paragrafo'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis soluta officiis, laborum facere rem veniam voluptate similique sequi totam quam, eveniet sapiente nostrum vel saepe corporis! Nostrum doloremque odit optio.</p>
+    </>
+  )
+}
+
+export default App
