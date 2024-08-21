4763c5c5fd7dbd55375ef4ca53fed7f9c6a7e01f
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2024-08-21 17:05:06 -0300

    add exemplo de componentização e img

---
+++ componentesreact/src/components/BarbieAriel.jsx
@@ -0,0 +1,13 @@
+import './BarbieAriel.css'
+
+function BarbieAriel() {
+  return (
+    <div className='barbieariel-container'>
+      <h2>Barbie Ariel</h2>
+      <img src="./Images/Ariel.webp" alt="" />
+
+    </div>
+  )
+}
+
+export default BarbieAriel
